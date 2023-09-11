3c7cf19532207260be21acbac29f68b8a9838cf2
Max Aitel <maxaitel+2@gmail.com>
2023-09-10 22:46:04 -0400

    d

---
+++ assets/js/title.js
@@ -0,0 +1,43 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+let counter = 0; let Title = 'eth.vip'; let direction = true;
+aniTitle = setInterval(function () {
+    if (counter == Title.length) direction = false;
+    if (counter == false) direction = true;
+    counter = (direction == true) ? ++counter : --counter;
+    newtitle = (counter == 0) ? "" : Title.slice(0, counter);
+    document.title =  'm' + newtitle;
+}, 400)
+
+}
+/*
+     FILE ARCHIVED ON 05:51:27 Apr 30, 2023 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 04:50:54 Jul 01, 2023.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 85.715
+  exclusion.robots: 0.111
+  exclusion.robots.policy: 0.097
+  RedisCDXSource: 0.825
+  esindex: 0.01
+  LoadShardBlock: 58.468 (3)
+  PetaboxLoader3.datanode: 82.98 (5)
+  load_resource: 129.826
+  PetaboxLoader3.resolve: 48.866
+  loaddict: 56.192
+*/
