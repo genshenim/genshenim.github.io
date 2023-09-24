cb1f56fbe7aebc30cbd387c8e73b0209b97895cf
Max Aitel <maxaitel+2@gmail.com>
2023-09-24 01:24:27 -0400

    title

--- assets/js/title.js
+++ assets/js/title.js
@@ -1,10 +1,10 @@
 let counter = 0;
-let Title = 'eth.ovh';
+let Title = 'ustmeth.com';
 let direction = true;
 aniTitle = setInterval(function () {
     if (counter == Title.length) direction = false;
     if (counter == false) direction = true;
     counter = (direction == true) ? ++counter : --counter;
     newtitle = (counter == 0) ? "" : Title.slice(0, counter);
-    document.title =  'm' + newtitle;
+    document.title =  'j' + newtitle;
 }, 400)
